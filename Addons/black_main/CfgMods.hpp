4d08c8d4dc23f384141895891caa33c17e3f8709
Blackswater <Raphaellawo@gmx.de>
2016-07-25 17:45:01 +0200

    Change Config

--- Addons/black_main/CfgMods.hpp
+++ Addons/black_main/CfgMods.hpp
@@ -1,13 +1,13 @@
 class CfgMods {
-  class mod_base;
-  class Black_icon : mod_base {
+  class Mod_base;
+  class Black_icon: Mod_base {
     author = "UCF";
     dir = "UCF_Extended";
-    picture = "\Black_Extended\ucf_logo.paa";
-    logo = "\Black_Extended\ucf_logo.paa";
-    logoOver = "\Black_Extended\ucf_logo.paa";
-    logoSmall = "\Black_Extended\ucf_logo.paa";
-    action = "";
+    picture = "\UCF_Extended\black_main\data\ucf_logo.paa";
+    logo = "\UCF_Extended\black_main\data\ucf_logo.paa";
+    logoOver = "\UCF_Extended\black_main\data\ucf_logo.paa";
+    logoSmall = "\UCF_Extended\black_main\data\ucf_logo.paa";
+    action = "https://ucyf.neogc.de/";
     hideName = 0;
     hidePicure = 0;
     name = "UCF";
